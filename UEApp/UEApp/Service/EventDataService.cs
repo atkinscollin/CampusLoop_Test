9c60ed13441cc7185e2e84c2cffac7f3ec270aec
Collin <blakfishy@gmail.com>
2017-04-20 00:26:09 -0400

    init

---
+++ UEApp/UEApp/Service/EventDataService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using Microsoft.WindowsAzure.MobileServices.SQLiteStore;
+using Microsoft.WindowsAzure.MobileServices.Sync;
+using System;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace UEApp
+{
+    public class EventDataService
+    {
+        public MobileServiceClient MobileService { get; set; } = null;
+        IMobileServiceSyncTable<Event> eventTable;
+
+        public async Task Initialize()
+        {
+            if (MobileService?.SyncContext?.IsInitialized ?? false)
+                return;
+
+            //Create our client
+            MobileService = new MobileServiceClient("http://campusloopservice.azurewebsites.net");
+
+            const string path = "syncstore.db";
+            //setup our local sqlite store and intialize our table
+            var store = new MobileServiceSQLiteStore(path);
+            store.DefineTable<Event>();
+
+            await MobileService.SyncContext.InitializeAsync(store, new MobileServiceSyncHandler());
+
+            //Get our sync table that will call out to azure
+            eventTable = MobileService.GetSyncTable<Event>();
+        }
+
+        public async Task<IEnumerable> GetEvents()
+        {
+            await Initialize();
+            await SyncEvent();
+            return await eventTable.OrderBy(c => c.Date).ToEnumerableAsync();
+        }
+
+        public async Task<Event> AddEvent(string title, string location, DateTime date, string photoURL, string tag1)
+        {
+            await Initialize();
+
+            //create and insert coffee
+            var freshEvent = new Event { Title = title, Location = location, Date = date, PhotoURL = photoURL, Tag1 = tag1 };
+
+            await eventTable.InsertAsync(freshEvent);
+
+            //Synchronize coffee
+            await SyncEvent();
+
+            return freshEvent;
+        }
+
+        public async Task SyncEvent()
+        {
+            //pull down all latest changes and then push current coffees up
+            await eventTable.PullAsync("allEvents", eventTable.CreateQuery());
+            await MobileService.SyncContext.PushAsync();
+        }
+    }
+}
---
+++ WebAPI/Models/Event.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Azure.Mobile.Server;
+using System;
+
+namespace UCEvents_WebAPI.Models
+{
+    public class Event : EntityData
+    {
+        public string Title { get; set; }
+        public string Location { get; set; }
+        public DateTime Date { get; set; }
+        public string PhotoURL { get; set; }
+        public string Tag1 { get; set; }
+    }
+}
